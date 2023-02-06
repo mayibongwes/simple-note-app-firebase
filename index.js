7eea98cf83eabbe8b7611b77c72c136a35c75de9
Mayibongwe Sibanda <mayibongwes@gmail.com>
2023-02-06 21:35:53 +0200

    First Commit
    Crud works with browser local storage

---
+++ index.js
@@ -0,0 +1,167 @@
+class Note {
+    constructor(id, title, text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.updated = Date.now();
+    }
+}
+
+class App{
+    constructor(){
+        this.notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+        /**
+         * Page Elements
+         */
+        this.$formTitle  = document.querySelector("#note-title");
+        this.$formText   = document.querySelector("#note-text");
+        this.$formButton = document.querySelector("#submit");
+        this.$notes = document.querySelector("#notes");
+
+        this.addEventListeners()
+        this.displayNotes()
+    }
+
+    addNote({ title, text }) {
+        if (text != "") {
+            const newNote = new Note(Date.now(), title, text);
+            this.notes = [...this.notes, newNote];
+            this.render();
+        }
+    }
+
+    editNote(id, { title, text }) {
+        this.notes = this.notes.map((note) => {
+            if (note.id == id) {
+                // note.title = title; don't want to change note title yet
+                note.text = text;
+            }
+            return note;
+        });
+        this.render();
+    }
+
+    deleteNote(id) {
+        this.notes = this.notes.filter((note) => note.id != id);
+        this.render();
+    }
+
+    addEventListeners(){
+        this.$formTitle.addEventListener("click", () => this.expandForm());
+        
+        this.$formButton.addEventListener("click", (event) => {
+            event.preventDefault()
+            const title = this.$formTitle.value;
+            const text = this.$formText.value;
+            this.addNote({ title, text });
+            this.hideControls(this.$formText,this.$formButton)
+            this.$formTitle.value = "";
+            this.$formText.value = "";
+        })
+    }
+
+    expandForm(){
+        this.$formTitle.placeholder = "Title";
+        this.showControls(this.$formText,this.$formButton)
+    }
+
+    collapseForm(){
+        this.$formTitle.placeholder = "Add Note";
+        this.hideControls(this.$formText,this.$formButton)
+    }
+
+    showControls(...controls){
+        controls.forEach((control) => {
+            control.classList.contains("hide") ? control.classList.remove("hide") : true
+            control.classList.contains("show") ? true : control.classList.add("show")
+        });
+    }
+
+    hideControls(...controls){
+        controls.forEach((control) => {
+            control.classList.contains("show") ? control.classList.remove("show") : true
+            control.classList.contains("hide") ? true : control.classList.add("hide")
+        });
+    }
+
+    saveNotes() {
+        localStorage.setItem('notes', JSON.stringify(this.notes));
+    }
+
+    render() {
+        this.saveNotes();
+        this.displayNotes();
+    }
+
+    editNoteEvent(event){
+        const noteDiv = event.target.closest('.note')
+        const pText = noteDiv.querySelector('p')
+        const cCheck = noteDiv.querySelector('.fa-check')
+        const contenteditable = pText.getAttribute('contenteditable')
+        if (!contenteditable || contenteditable == "false"){
+            pText.setAttribute('contenteditable', true)
+        }
+        event.target.style.display = "none";
+        cCheck.style.display = "block"
+    }
+
+    confirmNoteEdit(event){
+        const noteDiv = event.target.closest('.note')
+        const pText = noteDiv.querySelector('p')
+        const cPencil = noteDiv.querySelector('.fa-pencil')
+        const contenteditable = pText.getAttribute('contenteditable')
+        const noteId = noteDiv.querySelector('span').innerHTML
+        if (contenteditable || contenteditable == "true"){
+            pText.setAttribute('contenteditable', false)
+        }
+        event.target.style.display = "none";
+        cPencil.style.display = "block"
+        this.editNote(noteId,{title: "", text : pText.innerHTML})
+        this.render()
+    }
+
+    deleteNoteEvent(event){
+        const noteDiv = event.target.closest('.note')
+        const noteId = noteDiv.querySelector('span').innerHTML
+        this.deleteNote(noteId)
+        this.render()
+    }
+
+    /**
+     * edit icon: fa-pencil
+     * confirm icon: fa-check
+     * editable paragraph: contenteditable="true"
+     */
+    displayNotes() {
+        this.$notes.innerHTML = this.notes.map((note,idx) => {
+            const noteDate = new Date(note.updated)
+            const dateFormat = noteDate.getDate()      + "/"
+                             + (noteDate.getMonth()+1) + "/"
+                             + noteDate.getFullYear()  + " "
+                             + noteDate.getHours()     + ":"
+                             + noteDate.getMinutes()   + ":"
+                             + noteDate.getSeconds();
+            return `
+                ${idx > 0 ? "<hr />." : ""}
+                <div class="note">
+                    <span style="display:none">${note.id}</span>
+                    <h2 for="note1">${note.title}</h2>
+                    <div class="note-body">
+                        <p contenteditable="false">${note.text}</p>
+                        <div class="controls">
+                            <i class="fa-solid fa-pencil" onclick="app.editNoteEvent(event)"></i>
+                            <i class="fa-solid fa-check" style="display: none;" onclick="app.confirmNoteEdit(event)"></i>
+                            </a><i class="fa-solid fa-trash" onclick="app.deleteNoteEvent(event)"></i>
+                        </div>
+                    </div>
+                    <em>Updated: ${dateFormat}</em>
+                </div>
+            `
+        }).join("");
+    }
+
+}
+
+// const baseNote = new Note(1,"Sample Title","Body of the Note here")
+const app = new App();
